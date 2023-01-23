ace291725c0db558d88631b78f6980d16f86ec98
Paweł Chłąd <pchlad5@gmail.com>
2023-01-23 12:43:27 +0100

    Project init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::Read;
+use std::{env, vec};
+pub mod chip8;
+pub mod chip8display;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Provide path to chip-8 ROM");
+        std::process::exit(1);
+    }
+
+    let rom_path = &args[1];
+    let f = match File::open(rom_path) {
+        Ok(v) => v,
+        Err(v) => panic!("Could not open file: {}", v.to_string()),
+    };
+
+    let program_bytes: Vec<u8> = f
+        .bytes()
+        .map(|x| match x {
+            Ok(v) => v,
+            Err(_) => panic!("Error on program read"),
+        })
+        .collect();
+
+    let mut emu = chip8::Chip8Emulator::new(&program_bytes, chip8display::Chip8TerminalDisplay::new());
+    emu.run();
+}
